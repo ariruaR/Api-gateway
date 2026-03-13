314c30bff099f1d980f6e3b2e7fdb038473d6b4a
ariruaR <ariruarriuar@gmail.com>
2026-03-13 11:21:06 +0300

    test commit

--- api.py
+++ api.py
@@ -28,3 +28,21 @@ async def ping():
 # uvicorn Practice_EBBO.api:app --reload
 
 
+# deploy:
+  #   needs: [test]
+  #   runs-on: self-hosted
+
+  #   steps:
+  #     - uses: actions/checkout@v4
+
+  #     - name: Deploy FastAPI
+  #       run: |
+  #         cd /home/server/project
+  #         git pull
+  #         source venv/bin/activate
+  #         pip install -r requirements.txt
+  #         sudo systemctl restart fastapi  
+
+    
+
+
